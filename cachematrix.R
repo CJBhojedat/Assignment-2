e4b7241f4fba12704a3fe53bb9fc2797b65ca683
Carey B <cbhojedat@yahoo.com>
2015-04-26 17:23:35 -0400

    First Commit

---
+++ cachematrix.R
@@ -0,0 +1,43 @@
+# The Scoping rules of the R language can be manipulated 
+# to preserve state inside of an R object. R - functions can be written
+# to cache potentially time consuming and costly computations.
+# The functions below illustrates this.
+
+# makeCacheMatrix: This function creates a special "matrix" object
+# that can cache its inverse.
+# makeCacheMatrix creates a list containing a function to:
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+}
+
+
+# cacheSolve: This function computes the inverse of the special "matrix"
+# returned by makeCacheMatrix above. If the inverse has already been 
+# calculated (and the matrix has not changed), then the cachesolve should
+# retrieve the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
+}
+
